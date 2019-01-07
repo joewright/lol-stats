2be61acc1f87129fd5672abea4f5be2821a283dd
joewright <joewright87@gmail.com>
2019-01-06 22:43:13 -0500

    parse/map lol api results; finish up UI

--- server/index.js
+++ server/index.js
@@ -2,41 +2,204 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const async = require('async');
+const _ = require('lodash');
+
+// set up league of legends api client
+const {Kayn, REGIONS} = require('kayn');
+const kayn = Kayn(process.env.RIOT_LOL_API_KEY)({region: REGIONS.NORTH_AMERICA});
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// TODO: caching middleware
-app.post('/lol/matches', bodyParser.json(), (req, res) => {
+// TODO: set cache exipiration, use a DB
+// TODO: meaningful logging
+var cache = {};
+
+app.post('/lol/matches', bodyParser.json(), checkCache(), (req, res) => {
 	if (!req.body.summoner) {
-		return res.setStatus(422).send({
+		return res.status(422).send({
 			error: '"summoner" is required'
 		});
 	}
 
-	var durationMinutes = 44;
-	var totalCreepScore = 120;
-	var matches = [{
-		outcome: 'Victory',
-		durationMinutes: durationMinutes,
-		summoner: {
-			name: 'jimbo',
-			spells: ['fire', 'ice'],
-			runes: ['triangle'],
-			kda: '5/10/1',
-			items: ['potion', 'phoenix down'],
-			level: 64,
-			totalCreepScore: totalCreepScore,
-			creepPerMinute: Math.floor(totalCreepScore / durationMinutes)
-		},
-	}];
-
-	return res.send({
-		total: matches.length,
-		matches: matches
+	const username = req.body.summoner;
+	getMatchesByName(username, (err, result) => {
+		if (err) {
+			// TODO: some more useful error handling
+			var errorData = {error: 'Unable to find state'};
+			if (err.error && err.error.name) {
+				errorData.name = err.error.name;
+				errorData.message = err.error.message;
+			}
+			return res.status(404).send(errorData);
+		}
+		cache[username] = result;
+		res.send(cache[username]);
 	});
 });
 
 app.listen(port, () => {
 	console.log(`Running on port ${port}`);
 });
+
+function checkCache() {
+	return (req, res, next) => {
+		const username = req.body.summoner;
+		if (cache[username]) {
+			return res.send(cache[username]);
+		}
+		next();
+	};
+}
+
+function getMatchesByName(name, done) {
+	var context = {};
+
+	async.series([
+		// get summoner
+		function getSummoner(next) {
+			kayn.SummonerV4.by.name(name)
+				.then(summoner => {
+					context.summoner = summoner;
+					next();
+				})
+				.catch(next);
+		},
+		// get latest matches for context.summoner with details
+		function getMatches(next) {
+			kayn.MatchlistV4.getRecentMatchlistByAccountID(context.summoner.accountId)
+				.then(matchList => {
+					getMatchDetails(matchList, (err, details) => {
+						context.champions = details.champions;
+						context.matches = details.matches;
+						context.spells = details.spells;
+
+						next(err);
+					});
+				})
+				.catch(next);
+		}
+	], err => {
+		if (err) {
+			return done(err);
+		}
+
+		// map match details to summoner/champion
+		var result = {
+			matches: context.matches.map(match => {
+				return mapMatchWithContext(match, context);
+			})
+		};
+		done(err, result);
+	});
+}
+
+function mapMatchWithContext(match, context) {
+	var foundParticipant = _.find(match.participantIdentities, (pId) => {
+		return pId.player.accountId === context.summoner.accountId;
+	});
+	var participant = _.find(match.participants, {
+		participantId: foundParticipant.participantId
+	});
+
+	var champion = _.find(context.champions, {
+		id: '' + participant.championId
+	});
+
+	var hours = match.gameDuration / 3600;
+	var minutes = (match.gameDuration % 3600) / 60;
+	var seconds = match.gameDuration % 60;
+	var duration = [hours, minutes, seconds].map(formatTime).join(':');
+	// need this for creeps per minute
+	var totalMinutes = match.gameDuration / 60;
+
+	var items = _.chain(participant.stats)
+		.pick(['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6'])
+		.values()
+		.compact().value();
+
+	var spells = [participant.spell1Id, participant.spell2Id].map(id => {
+		return context.spells[id] || id;
+	});
+
+	// map the match data using the local data context
+	var mapped = {
+		outcome: participant.stats.win ? 'Victory' : 'Defeat',
+		duration: duration,
+		summoner: {
+			name: context.summoner.name,
+			spells: spells,
+			// TODO: find rune data for match; learn about runes in this game
+			runes: []
+		},
+		champion: {
+			name: champion.name,
+			level: participant.stats.champLevel
+		},
+		kda: [participant.stats.kills, participant.stats.deaths, participant.stats.assists].join('/'),
+		items: items,
+		// minion stats
+		totalCreepScore: participant.stats.totalMinionsKilled,
+		creepScorePerMinute: Math.floor(participant.stats.totalMinionsKilled / totalMinutes)
+	};
+	return mapped;
+}
+
+function formatTime(val) {
+	return ('0' + Math.floor(val)).slice(-2);
+}
+
+function getMatchDetails(matchList, done) {
+	var details = {
+		champions: [],
+		matches: [],
+		spells: {}
+	};
+	// list of champion IDs to retrieve
+	var champions = _.chain(matchList.matches)
+		.map('champion')
+		.uniq().value();
+
+	async.parallel([
+		// get champions
+		function getChampions(next) {
+			kayn.DDragon.Champion.listDataByIdWithParentAsId()
+				.then(championsById => {
+					details.champions = champions.map(champId => {
+						return championsById.data[champId];
+					});
+					next();
+				})
+				.catch(next);
+		},
+		// get spells
+		function getSpells(next) {
+			kayn.DDragon.SummonerSpell.list()
+				.then(spells => {
+					var spellsById = {};
+					_.values(spells.data).forEach(function(spell) {
+						spellsById[spell.key] = spell.name;
+					});
+					details.spells = spellsById;
+					next();
+				})
+				.catch(next);
+		},
+		// get match details
+		function getMatchDetails(next) {
+			async.eachLimit(matchList.matches, 2, (match, nextMatch) => {
+				kayn.MatchV4.get(match.gameId)
+					.then(matchDetail => {
+						_.defaults(matchDetail, match);
+						details.matches.push(matchDetail);
+						nextMatch();
+					})
+					.catch(nextMatch);
+			}, next);
+		}
+	], err => {
+		done(err, details);
+	});
+}
--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,12 +1,90 @@
 (function() {
 	'use strict';
+	// TODO: give up on plain js and use jsx
 	// createElement shortcut
 	const el = React.createElement;
 
 	// Define the Match container
 	class Match extends React.Component {
 		render() {
-			return el('div', null, `Summoner: ${this.props.match.summoner.name}`);
+			var divider = el('hr');
+			
+			var summoner = el('h3', null, this.props.match.summoner.name);
+			var outcome = el('p', null, `Outcome: ${this.props.match.outcome}`);
+			var duration = el('p', null, `Game Duration: ${this.props.match.duration}`);
+			
+			var spellsTitle = el('strong', null, 'Spells');
+			var spells = el('ul', null, this.props.match.summoner.spells.map(spell => {
+				return el('li', null, spell);
+			}));
+
+			var runes = el('p', null, [
+				el('strong', null, 'Runes:'), 
+				el('span', null, ' N/A')]);
+
+			var champion = el('div', null, [
+				el('strong', null, 'Champion:'), 
+				el('span', null, ` ${this.props.match.champion.name}, Level ${this.props.match.champion.level}`)]);
+
+			var kda = el('p', null, `KDA: ${this.props.match.kda}`);
+
+			var itemsTitle = el('strong', null, 'Items');
+			var items = el('ul', null, this.props.match.items.map(item => {
+				return el('li', null, item);
+			}));
+
+			var creepScore = el('p', null, `Total creep score: ${this.props.match.totalCreepScore}`);
+			var creepsPerMinute = el('p', null, `Creeps per minute: ${this.props.match.creepScorePerMinute}`);
+
+			var childComponents = [divider, summoner, outcome, duration, spellsTitle, 
+				spells, runes, champion, kda, creepScore, creepsPerMinute];
+
+			if (this.props.match.items.length) {
+				childComponents.push(itemsTitle);
+				childComponents.push(items);
+			}
+
+			return el('div', null, childComponents);
+		}
+	}
+
+	// Make a form to enter summoner name
+	class SummonerForm extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				// set a default summoner name
+				value: 'ayyjoewrong'
+			};
+			this.handleChange = this.handleChange.bind(this);
+		}
+
+		handleChange(event) {
+			this.setState({
+				value: event.target.value
+			});
+		}
+
+		handleSubmit() {
+			this.props.refresh(this.state.value);
+		}
+
+		render() {
+			var label = el('label', {
+				for: 'summoner'
+			}, 'Get stats for summoner: ');
+			var input = el('input', {
+				name: 'summoner',
+				value: this.state.value,
+				type: 'text',
+				onChange: this.handleChange
+			});
+			var reload = el(ReloadButton, {
+				refresh: () => {
+					this.handleSubmit();
+				}
+			});
+			return el('div', this.props, [label, input, reload]);
 		}
 	}
 
@@ -15,32 +93,34 @@
 		constructor(props) {
 			super(props);
 			this.state = {
-				matches: props.matches
+				matches: props.matches,
+				summoner: props.summoner
 			};
 		}
-		handleRefresh() {
-			getMatches((err, data) => {
+		handleRefresh(summoner) {
+			getMatches(summoner, (err, data) => {
 				this.state.matches = data.matches;
 			});
 		}
 		render() {
+			var summonerForm = el(SummonerForm, {
+				refresh: (summoner) => {
+					this.handleRefresh(summoner);
+				}
+			});
 			var matches = this.state.matches.map((match) => {
 				return el(Match, {
 					match: match
 				});
 			});
-			var reloadButton = el(ReloadButton, {
-				refresh: () => {
-					this.handleRefresh();
-				}
-			});
-			matches.unshift(reloadButton);
-			return el('div', null, matches);
+			matches = matches.concat(matches);
+			var components = [summonerForm, matches];
+			return el('div', null, components);
 		}
 	}
 
 	// get the initial match data
-	getMatches((err, data) => {
+	getMatches('ayyjoewrong', (err, data) => {
 		ReactDOM.render(
 			el(App, data, null),
 			document.getElementById('app'));
@@ -52,12 +132,12 @@
 		}, 'Refresh');
 	}
 
-	function getMatches(done) {
+	function getMatches(summoner, done) {
 		Request({
 			url: '/lol/matches',
 			method: 'POST',
 			data: {
-				summoner: 'Joe'
+				summoner: summoner
 			}
 		}, (err, data) => {
 			// TODO: better error handling
@@ -80,6 +160,12 @@
 			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
 				var data = JSON.parse(this.response);
 				done(null, data);
+			} else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200) {
+				done({
+					status: this.status,
+					error: 'Something blew up',
+					response: this.response
+				});
 			}
 		};
 		var data;
